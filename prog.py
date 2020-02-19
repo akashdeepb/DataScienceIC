fc626400d464a826dc24036f7a58ceae3f12bacc
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2020-02-19 04:51:59 -0500

    Added Max Function

--- prog.py
+++ prog.py
@@ -3,3 +3,6 @@ import pandas
 data = pandas.read_csv("crop_production.csv")
 df = pandas.DataFrame(data)
 print(df[df['Crop']=='Coconut '])       # Print all the Rows containing Coconut as Crop
+
+
+print(df[df['Crop']=='Arecanut'].max())     #Print Max of the Row with Crop as Arecanut
