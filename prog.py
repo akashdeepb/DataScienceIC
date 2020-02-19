25928777fb060611c83b0b4721f4ac9852292840
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2020-02-19 04:56:17 -0500

    Added Sum Function

--- prog.py
+++ prog.py
@@ -1,14 +1,15 @@
 import pandas
 
-#Read the CSV File present in the same Directory
+# Read the CSV File present in the same Directory
 data = pandas.read_csv("crop_production.csv")
 
-#Create a Data Frame from the Data imported from CSV File
+# Create a Data Frame from the Data imported from CSV File
 df = pandas.DataFrame(data)
 
 print(df)   # Print the content of the DataFrame
 
 print(df[df['Crop']=='Coconut '])       # Print all the Rows containing Coconut as Crop
 
+print(df[df['Crop']=='Arecanut'].max())     # Print Max of the Row with Crop as Arecanut
 
-print(df[df['Crop']=='Arecanut'].max())     #Print Max of the Row with Crop as Arecanut
+print(df['Area'].sum())     # Print the Sum of all the Area 
