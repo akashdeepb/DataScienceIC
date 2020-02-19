fa4394b4658a0692a6d508b9965d9dec3a27dd37
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2020-02-19 04:50:27 -0500

    Added Comment

--- prog.py
+++ prog.py
@@ -2,4 +2,4 @@ import pandas
 
 data = pandas.read_csv("crop_production.csv")
 df = pandas.DataFrame(data)
-print(df[df['Crop']=='Coconut '])
+print(df[df['Crop']=='Coconut '])       # Print all the Rows containing Coconut as Crop
