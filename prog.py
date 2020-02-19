a9e7126d86499d5b91ee097a0e9f65a0270c90b2
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2020-02-19 04:54:28 -0500

    Documented and Added Print All Content

--- prog.py
+++ prog.py
@@ -1,7 +1,13 @@
 import pandas
 
+#Read the CSV File present in the same Directory
 data = pandas.read_csv("crop_production.csv")
+
+#Create a Data Frame from the Data imported from CSV File
 df = pandas.DataFrame(data)
+
+print(df)   # Print the content of the DataFrame
+
 print(df[df['Crop']=='Coconut '])       # Print all the Rows containing Coconut as Crop
 
 
